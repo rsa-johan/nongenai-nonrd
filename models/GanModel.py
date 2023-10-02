06c6a300100d4b47a8f6d5af7f257a2cbe2ae92e
rsa-johan <quantumgravity.qg@gmail.com>
2023-10-02 18:59:01 +0530

    Added - Empty GAN

---
+++ models/GanModel.py
@@ -0,0 +1,10 @@
+from torch.nn import Module, Linear, LeakyReLU, ReLU
+
+def genblock(input_size, hidden_size):
+    pass
+
+def discblock(input_size, hidden_size):
+    pass
+
+class GAN(Module):
+    pass
